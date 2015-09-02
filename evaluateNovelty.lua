3d05bdb85484a934c0755d78ad2192a6054f341f
Dason Woodhouse <dwoodhouse@cmnhospitals.org>
2015-09-02 14:18:13 -0600

    adding novelty thing

---
+++ evaluateNovelty.lua
@@ -0,0 +1,21 @@
+function readCSV ()
+
+end
+
+function createNoveltyID
+local noveltyID = {
+	game_mode_id
+	final_level_id
+	death_count_distance_from_average
+	screen_distance_from_average = { h, v }
+	x_pos_distance_from_avg
+	y_post_distance_from_avg
+};
+if(game_mode == SMW.game_mode_overworld)
+
+
+end
+
+function EvaluateNovelty()
+
+end
