ba12dcd01a40377bfb159caff4500ac90c4415de
Dason Woodhouse <dwoodhouse@cmnhospitals.org>
2015-09-03 14:07:20 -0600

    Adding evaluateNovelty object.

--- evaluateNovelty.lua
+++ evaluateNovelty.lua
@@ -2,17 +2,18 @@ function readCSV ()
 
 end
 
-function createNoveltyID
-local noveltyID = {
-	game_mode_id
-	final_level_id
-	death_count_distance_from_average
-	screen_distance_from_average = { h, v }
-	x_pos_distance_from_avg
-	y_post_distance_from_avg
-};
-if(game_mode == SMW.game_mode_overworld)
+function createNoveltyID ()
+local noveltyID.final_game_mode_id, 
+		noveltyID.final_level_id, 
+		noveltyID.death_count_distance_from_average,
+		noveltyID.screen_distance_from_average.h,
+		noveltyID.screen_distance_from_average.v,
+		noveltyID.x_pos_distance_from_avg,
+		noveltyID.y_post_distance_from_avg
 
+if game_mode == SMW.game_mode_overworld then
+	noveltyID.final_game_mode_id = 2
+final_level_id
 
 end
 
